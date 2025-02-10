c545285b0db39383578150d37494e207d8536655
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-02-10 21:45:00 +0700

    Yes button FInale

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function selectOption(option) {
     if (option === 'yes') {
-        // Flash function
+        // Upload the cat
+        document.getElementById('question').style.display = 'none'; // Hide the question
+        document.getElementById('options').style.display = 'none';
+        displayYesCat();
+        document.getElementById('options').style.display = 'block';
+        document.getElementById('options').innerHTML = 'Yayyyyy I love youuuuu <3 ';
+        document.getElementById('options').style.fontFamily = 'Sacramento, cursive';
+        document.getElementById('options').style.fontSize = '57px';
     } 
     else if (option === 'no') {
         // Change text to "U sure?"
@@ -19,4 +26,14 @@ function selectOption(option) {
     }
 } 
 
+function displayYesCat() {
+    var imageContainer = document.getElementById('image-container');
 
+    var cat = new Image();
+    cat.src = 'giphy.gif'; 
+    cat.alt = 'happy-cat';
+    cat.onload = function() {
+        imageContainer.appendChild(cat);
+    };
+
+};
